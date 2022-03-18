c7fa8ce2ae24ec45dceb96b735b7bbdc2f2f5135
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-18 22:36:18 +0300

    add basic gatsby config

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+    title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "123"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
