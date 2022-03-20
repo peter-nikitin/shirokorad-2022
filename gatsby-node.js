82ce983fdc7066820dc2162e3c23dc04a7dd330b
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-20 11:43:26 +0300

    add node-api config

---
+++ gatsby-node.js
@@ -0,0 +1,37 @@
+const path = require(`path`);
+// Log out information after a build is done
+exports.onPostBuild = ({ reporter }) => {
+  reporter.info(`Your Gatsby site has been built!`);
+};
+// Create blog pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogPostTemplate = path.resolve(`${__dirname}/src/components/ProjectItem.tsx`);
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          id
+          slug
+          body
+          frontmatter {
+            title
+            stylist
+            photo
+          }
+        }
+      }
+    }
+  `);
+  result.data.allMdx.nodes.forEach((node) => {
+    createPage({
+      path: `${node.slug}`,
+      component: blogPostTemplate,
+      context: {
+        title: node.title,
+        slug: node.slug,
+        projectPhotos: `${node.slug}photos/*`
+      },
+    });
+  });
+};
