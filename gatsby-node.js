1941c3b22ada94922dd8cb965a108a5ec2460852
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-20 12:06:40 +0300

    fix temlate path

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ exports.onPostBuild = ({ reporter }) => {
 // Create blog pages dynamically
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
-  const blogPostTemplate = path.resolve(`${__dirname}/src/components/Detail.tsx`);
+  const blogPostTemplate = path.resolve(`${__dirname}/src/components/detail/Detail.tsx`);
   const result = await graphql(`
     query {
       allMdx {
