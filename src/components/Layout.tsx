9be81faad3bdf1a5486b84106a88cf5b74e137e5
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-21 10:40:36 +0300

    Add main paddings

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import { Link, useStaticQuery, graphql, PageProps } from "gatsby";
 import Logo from "./Logo";
+import styled from "styled-components";
 
 type Props = {
   pageTitle?: string;
   children: React.ReactElement;
 };
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  padding: 20px 30px;
+`;
 
 const Layout = ({ pageTitle, children }: Props) => {
   const data = useStaticQuery(graphql`
@@ -19,17 +25,16 @@ const Layout = ({ pageTitle, children }: Props) => {
   `);
 
   return (
-    <div>
+    <Wrapper>
       <title>
         {pageTitle} | {data.site.siteMetadata.title}
       </title>
-      <header>{data.site.siteMetadata.title}</header>
       <Logo />
       <main>
         <h1>{pageTitle}</h1>
         {children}
       </main>
-    </div>
+    </Wrapper>
   );
 };
 
