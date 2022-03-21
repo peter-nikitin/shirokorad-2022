04bc5d85de02c1f0f5656318134d8ba0c1ba0f60
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-21 10:40:25 +0300

    Add styling and link to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
+import { Link } from "gatsby";
+import React from "react";
+import styled from "styled-components";
+
+const LogoWrapper = styled.header`
+  text-align: center;
+`;
 
 const Logo = () => {
   return (
-    <div>Наталья Широкорад</div>
-  )
-}
+    <LogoWrapper>
+      <Link to="/">
+        <header>Наталья Широкорад</header>
+      </Link>
+    </LogoWrapper>
+  );
+};
 
-export default styled(Logo)`
-text-align: center;
-`
+export default Logo;
