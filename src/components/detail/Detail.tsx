5f8179aa3131b90e0b4b2742fe411de18b8d6e04
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-23 23:04:20 +0300

    Remove waste

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -7,16 +7,14 @@ import DetailPhoto from "./DetailPhoto";
 import Helmet from "react-helmet";
 import styled from "styled-components";
 import DetailGrid from "./DetailGrid";
+import DetailLightbox from "./DetailLightbox";
 
 const Detail = ({ data }: IDetail.RootObject) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <>
-        <Helmet>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/1.1.9/js/libs/jquery-1.10.2.min.js" />
-        </Helmet>
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
-
+        
         <DetailGrid photos={data.allFile.nodes} />
       </>
     </Layout>
