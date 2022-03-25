3d426d51bc3e057d12bec602ca9679674e44748c
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-25 09:12:54 +0300

    update pages

---
+++ src/pages/contacts.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const contacts = () => {
+  return (
+    <Layout>
+      <>
+        <h1>Контакты</h1> <p>Надо заполнить</p>
+      </>
+    </Layout>
+  );
+};
+
+export default contacts;
