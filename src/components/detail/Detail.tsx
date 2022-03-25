de97692c9c6bdccefb9c20521d2d8f172fe5c8ac
Petr Nikitin <nikitin.p.94@gmail.com>
2022-03-25 08:43:17 +0300

    Update layout

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -3,18 +3,44 @@ import Layout from "../Layout";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
 import { IDetail } from "../../types";
-import DetailPhoto from "./DetailPhoto";
-import Helmet from "react-helmet";
-import styled from "styled-components";
 import DetailGrid from "./DetailGrid";
-import DetailLightbox from "./DetailLightbox";
+import styled from "styled-components";
+import Scheme from "./Scheme";
+
+const Description = styled.div`
+  display: flex;
+  max-width: 1000px;
+  justify-content: center;
+  margin: 30px auto 70px auto;
+`;
+
+const TextWrapper = styled.article`
+  padding: 20px 20px 20px 50px;
+  width: 65ch;
+`;
 
 const Detail = ({ data }: IDetail.RootObject) => {
   return (
-    <Layout pageTitle={data.mdx.frontmatter.title}>
+    <Layout>
       <>
-        <MDXRenderer>{data.mdx.body}</MDXRenderer>
-        
+        <Description>
+          <Scheme
+            schemeImage={
+              data.mdx.frontmatter.scheme.childImageSharp.gatsbyImageData
+            }
+          />
+          <TextWrapper>
+            <h1>{data.mdx.frontmatter.title}</h1>
+            <MDXRenderer>{data.mdx.body}</MDXRenderer>
+            {data.mdx.frontmatter.stylist && (
+              <p>Стилист: {data.mdx.frontmatter.stylist}</p>
+            )}
+            {data.mdx.frontmatter.photo && (
+              <p>Фотограф: {data.mdx.frontmatter.photo}</p>
+            )}
+          </TextWrapper>
+        </Description>
+
         <DetailGrid photos={data.allFile.nodes} />
       </>
     </Layout>
@@ -32,13 +58,20 @@ export const query = graphql`
       nodes {
         relativePath
         childrenImageSharp {
-          gatsbyImageData
+          gatsbyImageData(width: 800)
         }
       }
     }
     mdx(slug: { eq: $slug }) {
       frontmatter {
         title
+        photo
+        stylist
+        scheme {
+          childImageSharp {
+            gatsbyImageData(width: 850)
+          }
+        }
       }
       slug
       body
